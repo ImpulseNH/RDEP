03a40c01f2ee61818f569764c5cbfad8df507fee
monserratcornejo21 <monserrat.cornejo.c@mail.pucv.cl>
2022-05-24 16:35:41 -0400

    monse

--- frontend/src/app/components/formulario-registro/formulario-registro.component.ts
+++ frontend/src/app/components/formulario-registro/formulario-registro.component.ts
@@ -13,7 +13,7 @@ export class FormularioRegistroComponent implements OnInit {
   registro:boolean=false;
   constructor(private fb:FormBuilder,  private router:Router) {
     this.formulario=this.fb.group({
-      rut:["",[Validators.required, Validators.pattern('[0-9]{7,8}-[kK|0-9]{1}')]],
+      rut:['',[Validators.required, Validators.pattern('[0-9]{7,8}-[kK|0-9]{1}')]],
       nombre:['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
       email:['', [Validators.required, Validators.email, Validators.maxLength(100)]],
       telefono:['', [Validators.required, Validators.minLength(1), Validators.maxLength(9)]],
